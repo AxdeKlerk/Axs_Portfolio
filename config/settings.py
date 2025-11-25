100f96b867147cc6edb0f798ae9dc96d75bec445
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-25 15:31:29 +0000

    Change database back to sqlite3 in settings.py

--- config/settings.py
+++ config/settings.py
@@ -95,22 +95,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database configuration
-DATABASE_URL = env("DATABASE_URL", default=None)
-
-if DATABASE_URL:
-    DATABASES = {
-        "default": dj_database_url.parse(
-            DATABASE_URL,
-            conn_max_age=600,
-        )
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
