13ccb4b7665edca8b3da43d7eba4db42a3e7198c
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-11-26 09:47:58 +0000

    Add home_page probe

--- config/error_probe.py
+++ config/error_probe.py
@@ -9,3 +9,16 @@ def probe(request):
             "<pre>" + traceback.format_exc() + "</pre>",
             content_type="text/html"
         )
+
+def homepage_probe(request):
+    from django.http import JsonResponse
+    try:
+        data = {
+            "about": str(request.context.get("about", None)) if hasattr(request, "context") else "no context",
+            "page": "home",
+            "status": "view works"
+        }
+        return JsonResponse(data)
+    except Exception as e:
+        import traceback
+        return HttpResponse("<pre>" + traceback.format_exc() + "</pre>")
--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .error_probe import probe
+from .error_probe import probe, homepage_probe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
     path("probe/", probe),  # TEMPORARY!!
+    path("probe-home/", homepage_probe),
+
 ]
 
 if settings.DEBUG:
