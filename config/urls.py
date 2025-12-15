0cfe54e0288359de94bbb6769933660dabe8b359
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-12-15 09:31:07 +0000

    Remove obsolete debug views and clean config routing

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from config.views import custom_404, custom_500, force_500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portfolio.urls')),
-    path("force-500/", force_500),
 ]
 
 if settings.DEBUG:
